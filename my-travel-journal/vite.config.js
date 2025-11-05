4ce8c593f7d57a8c0edd103b650daa0d844256df
Anastasia Pravylo <anastasiapravylo@MacBook-Air---Anastasiia.local>
2025-11-05 16:34:33 +0200

    rebuild after adding .nojekyll

--- my-travel-journal/vite.config.js
+++ my-travel-journal/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/work/', // ← назва твого репозиторію
+  build: {
+    outDir: 'docs' // ← сюди збирається твій готовий сайт
+  }
 })
